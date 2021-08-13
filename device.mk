d34bd963e939d7b1861efa129411c6425cb737ca
Nauval Rizky <enuma.alrizky@gmail.com>
2021-08-13 10:17:40 +0700

    dipper: rootdir: Add script for exTHmUI Game-Mode Performance control
    This is based on samples at exTHmUI-supported device (link down below),
    but without turing-complete language involvements since we're only
    setting up sysfs nodes.
    
    [fryevia: correct wrong thermal value and add preset for inactive mode]
    
    Link: exthmui-devices/android_device_xiaomi_vince@dc18f8e
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <valkry24@gmail.com>
    Signed-off-by: alanndz <alanndz@qmail.id>
    Change-Id: Ife456f603d8b49f7ac36b05d7644beae3edb2986

--- device.mk
+++ device.mk
@@ -60,7 +60,8 @@ PRODUCT_PACKAGES += \
 
 # Device init scripts
 PRODUCT_PACKAGES += \
-    init.target.rc
+    init.target.rc \
+    init.performance_level.rc
 
 # Fingerprint
 PRODUCT_PACKAGES += \
